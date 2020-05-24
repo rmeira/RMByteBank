b3b45e1defa9c38b87cefb0306e87d8000fc2734
Rafael Meira <rafaelmeira@me.com>
2020-05-24 00:42:19 -0300

    update app and continue tomorow

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(FlutterInitApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FlutterInitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        body: TransferForm(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class TransferForm extends StatelessWidget {
+  final TextEditingController _account = TextEditingController();
+  final TextEditingController _value = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                controller: _account,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                decoration: InputDecoration(
+                  suffixIcon: Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 4.0),
+                    child:
+                        Icon(Icons.tonality), // myIcon is a 48px-wide widget.
+                  ),
+                  labelText: "Número da conta",
+                  hintText: '0000',
+                  border: OutlineInputBorder(),
+                  fillColor: Colors.orange[600],
+                ),
+                autofocus: true,
+                keyboardType: TextInputType.number,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                controller: _value,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                decoration: InputDecoration(
+                  suffixIcon: Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 4.0),
+                    child: Icon(
+                        Icons.attach_money), // myIcon is a 48px-wide widget.
+                  ),
+                  labelText: "Valor gasto",
+                  hintText: '120.00',
+                  border: OutlineInputBorder(),
+                ),
+                autofocus: true,
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              color: Colors.orange[600],
+              textColor: Colors.white,
+              child: Text("Adicionar transferência"),
+              onPressed: () {
+                final double value = double.tryParse(_value.text);
+                final int account = int.tryParse(_account.text);
+
+                if (value != null && account != null) {
+                  Transfer(value, account);
+                }
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(
+        actions: <Widget>[BackButton()],
+        title: const Text('Adicionar nova transferência'),
+        backgroundColor: Colors.orange[600],
+      ),
     );
   }
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TransferItem(Transfer(100.0, 1000)),
+            TransferItem(Transfer(100.0, 2000)),
+            TransferItem(Transfer(100.0, 3000)),
+            TransferItem(Transfer(100.0, 4000)),
+            TransferItem(Transfer(100.0, 5000)),
+            TransferItem(Transfer(100.0, 6000)),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Transferências'),
+        backgroundColor: Colors.orange[600],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.orange[600],
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(
+          Icons.monetization_on,
+          size: 32.00,
+        ),
+        title: Text('R ' + _transfer.value.toString()),
+        subtitle: Text('Conta: ' + _transfer.account.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int account;
+
+  Transfer(this.value, this.account);
 }
